84e60d219e04e484ab7d220a26e90cb9749b19b5
Domi0620 <domiciano.rincon@correounivalle.edu.co>
2019-01-31 19:22:55 -0500

    Initial commit

---
+++ app/src/main/java/appmoviles/com/copia/MainActivity.java
@@ -0,0 +1,17 @@
+package appmoviles.com.copia;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
