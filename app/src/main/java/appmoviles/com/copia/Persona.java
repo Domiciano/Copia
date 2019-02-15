31da252f435c74ba3b89b0f9136a4cea1f2fe277
Domi0620 <domiciano.rincon@correounivalle.edu.co>
2019-02-14 20:09:29 -0500

    RecyclerView

---
+++ app/src/main/java/appmoviles/com/copia/Persona.java
@@ -0,0 +1,13 @@
+package appmoviles.com.copia;
+
+public class Persona {
+    public String nombre;
+    public String numero;
+
+    public Persona(){}
+
+    public Persona(String nombre, String numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+}
